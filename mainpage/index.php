b1b86efdd5316cf85a4f8df256a66d3ee29fa0bf
stmmarvin <marvinakpabot@gmail.com>
2024-02-05 16:29:54 +0100

    contact form en information pagina gelinkt

--- mainpage/index.php
+++ mainpage/index.php
@@ -7,13 +7,11 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="hamburger-menu"><div class="rectangle">
+    <div class="menu"><div class="rectangle">
         <ul>
-            <li><a href="homepage.html">HOMEPAGE</a></li>
-            <li><a href="information.html">INFORMATION</a></li>
-            <li><a href="form.html">FORM</a></li>
-            <li><a href="contact.html">CONTACT</a></li>
-            <li><a href="behandelingen.html">BEHANDELINGEN</a></li>
+            <li><a href="InfoPage/information.php">INFORMATION</a></li>
+            <li><a href="Form/form.php">FORM</a></li>
+            <li><a href="contact/contact.php">CONTACT</a></li>
           </ul>
     </div></div>
 </body>
