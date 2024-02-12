d80eafa897967bc7fe181dedf54b7fa0685b928a
Luuk van den Bosch <336216@student.mboutrecht.nl>
2024-02-12 15:28:37 +0100

    main styling

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet"  href="css/style.css" />
+</head>
+<body>
+   
+<div class="navbar-main">
+    <img class="logo" src="../images/logo.png" alt="" width="90" height="90">
+    <div class="hamburger-menu">
+        <div class="rectangle">
+            <ul class="navbar" id="nav">
+                <li><a href="homepage.html" id="homepage">HOMEPAGE</a></li>
+                <li><a href="information.html">INFORMATION</a></li>
+                <li><a href="form.html">FORM</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+                <li><a class="behandelingen" href="behandelingen.html">BEHANDELINGEN</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+</html>
