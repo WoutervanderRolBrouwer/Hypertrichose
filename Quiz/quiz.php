7afc6cbf202b0d30fe93fbb5b3bacf2eeae158da
stmmarvin <marvinakpabot@gmail.com>
2024-02-29 10:17:46 +0100

    Quiz Pagina aangepast.

--- Quiz/quiz.php
+++ Quiz/quiz.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hypertrichose</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../Quiz/Styling/quiz.css">
 </head>
 <body>
     <div class="header">
@@ -19,69 +22,90 @@
          
         
         <form action="quiz_result.php" method="post">
+
+        <div class="question-block">
         <p>1. Wat is een ander woord voor het weerwolvensyndroom?</p>
         <input type="radio" name="question1" value="a"> Hypertrichose<br>
         <input type="radio" name="question1" value="b"> Vitiligo<br>
         <input type="radio" name="question1" value="c"> Albinisme<br>
         <input type="radio" name="question1" value="d"> Dermatillomanie<br><br>
+        </div>
 
+        <div class="question-block">
         <p>2. Wat veroorzaakt het weerwolvensyndroom?</p>
         <input type="radio" name="question2" value="a"> Bacteriële infectie<br>
         <input type="radio" name="question2" value="b"> Genetische mutatie<br>
         <input type="radio" name="question2" value="c"> Allergische reactie<br>
         <input type="radio" name="question2" value="d"> Hormonale onbalans<br><br>
-
+        </div>
+        
+        <div class="question-block">
         <p>3. Welk type weerwolvensyndroom wordt gekenmerkt door overmatige haargroei over het hele lichaam? </p>
         <input type="radio" name="question3" value="a"> Congenitale hypertrichose<br>
         <input type="radio" name="question3" value="b"> Acquired hypertrichosis<br>
         <input type="radio" name="question3" value="c"> Hirsutism<br>
         <input type="radio" name="question3" value="d"> Trichotillomanie <br><br>
+        </div>
 
+        <div class="question-block">
         <p>4. Welk orgaan wordt vaak aangetast door de symptomen van het weerwolvensyndroom? </p>
         <input type="radio" name="question4" value="a"> Nieren<br>
         <input type="radio" name="question4" value="b"> Lever<br>
         <input type="radio" name="question4" value="c"> Hart<br>
         <input type="radio" name="question4" value="d"> Huid<br><br>
+        </div>
 
+        <div class="question-block">
         <p>5. Welke van de volgende is een mogelijke oorzaak van het verworven weerwolvensyndroom?</p>
         <input type="radio" name="question5" value="a"> Genetische mutatie<br>
         <input type="radio" name="question5" value="b"> Medicijnen<br>
         <input type="radio" name="question5" value="c"> Allergieën<br>
         <input type="radio" name="question5" value="d"> Gebrek aan hygiëne<br><br>
+        </div>
 
+        <div class="question-block">
         <p>6. Hoe wordt het weerwolvensyndroom genoemd wanneer het alleen op bepaalde delen van het lichaam voorkomt?</p>
         <input type="radio" name="question6" value="a"> Localized hypertrichosis<br>
         <input type="radio" name="question6" value="b"> Generalized hypertrichosis<br>
         <input type="radio" name="question6" value="c"> Acquired hypertrichosis<br>
         <input type="radio" name="question6" value="d"> Congenital hypertrichosis<br><br>
+        </div>
 
+        <div class="question-block">
         <p>7. Welk van de volgende syndromen wordt soms verward met het weerwolvensyndroom vanwege overmatige haargroei?</p>
         <input type="radio" name="question7" value="a"> Cushing-syndroom<br>
         <input type="radio" name="question7" value="b"> Downsyndroom<br>
         <input type="radio" name="question7" value="c"> Turner-syndroom<br>
         <input type="radio" name="question7" value="d"> Marfansyndroom<br> <br>
+        </div>
 
+        <div class="question-block">
         <p>8. Welke van de volgende is GEEN methode voor de behandeling van het weerwolvensyndroom?</p>
         <input type="radio" name="question8" value="a"> Laserontharing<br>
         <input type="radio" name="question8" value="b"> Hormoontherapie<br>
         <input type="radio" name="question8" value="c"> Chemotherapie<br>
         <input type="radio" name="question8" value="d"> Elektrolyse<br><br>
+        </div>
 
+        <div class="question-block">
         <p>9. Welk hormoon speelt een belangrijke rol bij de regulering van haargroei en kan worden betrokken bij het weerwolvensyndroom?</p>
         <input type="radio" name="question9" value="a"> Oestrogeen<br>
         <input type="radio" name="question9" value="b"> Testosteron<br>
         <input type="radio" name="question9" value="c"> Insuline<br>
         <input type="radio" name="question9" value="d"> Cortisol<br><br>
+        </div>
 
+        <div class="question-block">
         <p>10. Welk deel van het lichaam is vaak het meest getroffen door overmatige haargroei bij het weerwolvensyndroom?</p>
         <input type="radio" name="question10" value="a"> Rug<br>
         <input type="radio" name="question10" value="b"> Armen<br>
         <input type="radio" name="question10" value="c"> Gezicht<br>
         <input type="radio" name="question10" value="d"> Buik<br><br>
+        </div>
         
-
-
         <input type="submit" value="Indienen">
+        
+        
     </div>
 </body>
 </html>
--- Quiz/quiz_result.php
+++ Quiz/quiz_result.php
@@ -1,36 +1,91 @@
-<?php
-    // Controleer of het formulier is ingediend
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Array met de juiste antwoorden
-        $correct_answers = array(
-            "question1" => "a",
-            "question2" => "b",
-            "question3" => "a",
-            "question4" => "d",
-            "question5" => "b",
-            "question6" => "a",
-            "question7" => "a",
-            "question8" => "c",
-            "question9" => "b",
-            "question10"=> "c",
-        );
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Resultaat</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../Quiz/Styling/quiz.css">
+    <link rel="stylesheet" href="../Quiz\Styling\quiz_result.css">
+</head>
+<body>
 
-        // Initialiseer score
-        $score = 0;
+    <div class="result">
+        <h1>Quiz Resultaat</h1>
+        <?php
+        // Controleer of de quiz is ingediend
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Simuleer de correcte antwoorden (normaal gesproken zou dit uit een database komen of elders worden opgeslagen)
+            $correct_answers = array(
+                "a", // Vraag 1
+                "b", // Vraag 2
+                "a", // Vraag 3
+                "d", // Vraag 4
+                "b", // Vraag 5
+                "a", // Vraag 6
+                "a", // Vraag 7
+                "c", // Vraag 8
+                "b", // Vraag 9
+                "c"  // Vraag 10
+            );
 
-        // Loop door de ingediende antwoorden en controleer ze met de juiste antwoorden
-        foreach ($correct_answers as $question => $correct_answer) {
-            if (isset($_POST[$question]) && $_POST[$question] == $correct_answer) {
-                $score++;
-            }
+           // Array om de antwoorden van de gebruiker op te slaan
+$user_answers = array();
+
+// Controleer of de array-sleutels zijn ingesteld voordat je ze gebruikt
+if(isset($_POST["question1"])) {
+    $user_answers[] = $_POST["question1"];
+}
+if(isset($_POST["question2"])) {
+    $user_answers[] = $_POST["question2"];
+}
+if(isset($_POST["question3"])) {
+    $user_answers[] = $_POST["question3"];
+}
+if(isset($_POST["question4"])) {
+    $user_answers[] = $_POST["question4"];
+}
+if(isset($_POST["question5"])) {
+    $user_answers[] = $_POST["question5"];
+}
+if(isset($_POST["question6"])) {
+    $user_answers[] = $_POST["question6"];
+}
+if(isset($_POST["question7"])) {
+    $user_answers[] = $_POST["question8"];
+}
+if(isset($_POST["question8"])) {
+    $user_answers[] = $_POST["question8"];
+}
+if(isset($_POST["question9"])) {
+    $user_answers[] = $_POST["question9"];
+}
+if(isset($_POST["question10"])) {
+    $user_answers[] = $_POST["question10"];
+}
+// Voeg vergelijkbare controles toe voor de andere vragen...
+
+// Loop door de vragen en vergelijk de antwoorden
+for ($i = 0; $i < count($correct_answers); $i++) {
+    $question_number = $i + 1;
+    echo "<p>Vraag $question_number: ";
+    if (isset($user_answers[$i])) {
+        if ($user_answers[$i] === $correct_answers[$i]) {
+            echo "<span class='correct-answer'>Jouw antwoord is correct!</span>";
+        } else {
+            echo "<span class='incorrect-answer'>Jouw antwoord is fout. Het juiste antwoord is: " . $correct_answers[$i] . "</span>";
         }
-
-        // Toon de score aan de gebruiker
-        echo "<p>Je score is: $score / " . count($correct_answers) . "</p>";
     } else {
-        // Als het formulier niet is ingediend, toon een foutmelding
-        echo "<p>Er is een fout opgetreden. Probeer het opnieuw.</p>";
+        echo "<span class='incorrect-answer'>Je hebt deze vraag niet beantwoord.</span>";
     }
-    ?>
+    echo "</p>";
+}
 
-    <p><a href="quiz.php">Terug naar de quiz</a></p>
+        } else {
+            echo "<p>De quiz is niet ingediend.</p>";
+        }
+        ?>
+
+</body>
+</html>
