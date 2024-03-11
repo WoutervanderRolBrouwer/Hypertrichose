485a22e22c68a3ee7f74559984e5d32b32f0671e
estellemsastro <estelle2007-sastropawiro@outlook.com>
2024-03-11 15:56:33 +0100

    ik heb de behandelingen afgemaakt

---
+++ behandelingen/behandeling.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="nl"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Behandelingen, ​Vastelling van Hypertrichose, De behandeling, ​Wat kan jij doen​, ​uiteindelijke resultaat">
+    <meta name="description" content="">
+    <title>behandeling</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="behandeling.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 6.5.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link rel="stylesheet" href="../css/style.css">
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": ""
+}</script>
+
+<header>
+  <nav class="main-menu">
+      <img src="../logo/logo.svg" height="90px" alt="">
+      <ul >
+              <li><a class="home" href="../index.php" id="home">HOME</a></li>
+              <li><a class="information" href="../infopage/information.php">INFORMATION</a></li>
+              <li><a class="form" href="../form/form.php">FORM</a></li>
+              <li><a class="contact" href="../contact/contact.php">CONTACT</a></li>
+              <li><a class="behandelingen" href="../behandelingen/behandeling.php">BEHANDELINGEN</a></li>
+      </ul>
+  </nav>
+</header>
+
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="behandeling">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-path-to-root="./" data-include-products="false" class="u-body u-grey-5 u-xl-mode" data-lang="nl">
+    <section class="u-clearfix u-section-1" id="sec-4c38">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="data-layout-selected u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-col">
+              <div class="u-size-30">
+                <div class="u-layout-col">
+                  <div class="u-container-style u-image u-layout-cell u-size-60 u-image-1" data-image-width="1170" data-image-height="728">
+                    <div class="u-container-layout u-container-layout-1">
+                      <h1 class="u-align-center u-text u-text-default u-text-grey-5 u-title u-text-1">Behandelingen</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="u-size-30">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-grey-15 u-layout-cell u-size-30 u-layout-cell-2">
+                    <div class="u-container-layout u-valign-bottom u-container-layout-2">
+                      <h2 class="u-align-center u-text u-text-default u-text-2"> Vastelling van Hypertrichose</h2>
+                      <p class="u-align-center u-text u-text-3">Wanneer artsen denken dat iemand&nbsp; hypertrischose heeft, kijken ze naar hoe het eruitziet en hoe het zich gedraagt. Als ze denken dat er andere gezondheidprobleem achter zit, zoals een ziekte of syndroom, sturen ze de persoon door naar een andere dokters die meer gespecialiseerd zijn, zoals kinderartsen of internisten. </p>
+                    </div>
+                  </div>
+                  <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-3">
+                    <div class="u-container-layout u-container-layout-3">
+                      <h2 class="u-text u-text-default u-text-4">De behandeling</h2>
+                      <p class="u-align-center u-text u-text-5">Depilatie methoden zijn methods om haar te verwijderen, zoals elektrische epilatie en laserepilatie, verwijderen zoals scheren,harsen en het gebruik van ontharing crémes, worden gebruikt om haar tijdelijk te verwijderen. Aan de andere kant , epilatiemethoden zoals elektrische epilatie en laserepilatie, verwijderen haarzakjes permanent met behulp van stroom of laserlicht. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-container-align-center u-section-2" id="sec-6faf">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="data-layout-selected u-clearfix u-expanded-width u-gutter-50 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-left u-container-style u-layout-cell u-left-cell u-shape-rectangle u-size-30 u-layout-cell-1">
+                <div class="u-container-layout u-valign-top u-container-layout-1">
+                  <div class="custom-expanded u-border-2 u-border-grey-60 u-video u-video-1">
+                    <div class="embed-responsive embed-responsive-1">
+                      <iframe style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;" class="embed-responsive-item" src="https://www.youtube.com/embed/YuewRtRSJGM?playlist=YuewRtRSJGM&amp;loop=1&amp;mute=1&amp;showinfo=0&amp;controls=0&amp;start=0&amp;autoplay=1" data-autoplay="1" frameborder="0" allowfullscreen=""></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-grey-15 u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2">
+                  <h2 class="u-align-center u-text u-text-1"> Wat kan jij doen </h2>
+                  <p class="u-align-center u-text u-text-2">Haar groei kan optreden bij de geboorte maar ook bij moedervlekken,rug en wenkbrouw.&nbsp;<br>
+                    <br>Als je zorgen maakt over te veel haar op je lichaam, is&nbsp; het belangrijk om eerst je huisarts te raadplegen. Het is meestal niet verstandig om zelf te proberen de situatie te behandelen, omdat er misschien een onderliggende reden is voor het probleem. Je kunt meer leren over de voor- en nadelen van verschillende methoden om haar te verwijderen in het gedeelte genaamd "wat is de behandeling"
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-container-align-center u-grey-5 u-section-3" id="sec-df5a">
+      <div class="u-clearfix u-sheet u-valign-top u-sheet-1">
+        <div class="data-layout-selected u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout" style="">
+            <div class="u-layout-row" style="">
+              <div class="u-container-style u-grey-15 u-layout-cell u-left-cell u-size-31 u-size-xs-60 u-layout-cell-1" src="">
+                <div class="u-container-layout u-container-layout-1">
+                  <h2 class="u-align-center u-text u-text-1"> uiteindelijke resultaat</h2>
+                  <p class="u-align-center u-text u-text-2">Hypertrichose gaat meestal niet vanzelf over en heeft vaak behandeling nodig als een medicijn de oorzaak is, kan de overbeharing als je stopt met medicijn.</p>
+                </div>
+              </div>
+              <div class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-29 u-size-xs-60 u-image-1" src="" data-image-width="390" data-image-height="272">
+                <div class="u-container-layout u-valign-top u-container-layout-2" src=""></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    
+    
+    <section class="u-backlink u-clearfix u-grey-80">
+    </section>
+  
+</body></html>
