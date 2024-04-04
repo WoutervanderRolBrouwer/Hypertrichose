9f2e5ba7b139478e9ef449223550c5010ce6748d
stmmarvin <marvinakpabot@gmail.com>
2024-04-04 10:00:55 +0200

    final commit

--- contact/config/config.php
+++ contact/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // Databaseverbinding parameters
 define('servername', 'localhost'); // Locatie van de database (meestal localhost)
-define('username', 'projectcontact-2024'); // Gebruikersnaam voor de database
-define('password', '!T-WJWQA!hTnhxR['); // Wachtwoord voor de database
-define('DB_NAME', 'projectcontactsql'); // Naam van de database
+define('username', 'root'); // Gebruikersnaam voor de database
+define('password', ''); // Wachtwoord voor de database
+define('DB_NAME', 'hypertrichose'); // Naam van de database
 
